8f776ec833bd2428c659f94bea31d1631c83012a
TKone7 <tobias.koller@protonmail.com>
2025-08-29 09:09:45 +0200

    formatting

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,85 @@
-import { parseSlip24, type Slip24 } from './slip24';
-import { isBoolean, isLiteral, isNull, isNullish, isNumber, isObject, isOneOf, isString } from './utils';
+import { parseSlip24, type Slip24 } from "./slip24";
+import {
+  isBoolean,
+  isLiteral,
+  isNull,
+  isNullish,
+  isNumber,
+  isObject,
+  isOneOf,
+  isString,
+} from "./utils";
 
-export { type Slip24 } from './slip24';
+export { type Slip24 } from "./slip24";
 
 export enum MessageVersion {
-  V0 = '0',
+  V0 = "0",
 }
 
 export enum V0MessageType {
-  RequestAddress = 'requestAddress',
-  RequestExtendedPublicKey = 'requestExtendedPublicKey',
-  VerifyAddress = 'verifyAddress',
-  Address = 'address',
-  ExtendedPublicKey = 'extendedPublicKey',
-  PaymentRequest = 'paymentRequest',
-  Close = 'close',
+  RequestAddress = "requestAddress",
+  RequestExtendedPublicKey = "requestExtendedPublicKey",
+  VerifyAddress = "verifyAddress",
+  Address = "address",
+  ExtendedPublicKey = "extendedPublicKey",
+  PaymentRequest = "paymentRequest",
+  Close = "close",
 }
 
 export enum V0MessageScriptType {
-  P2PKH = 'p2pkh',
-  P2WPKH = 'p2wpkh',
-  P2SH = 'p2sh',
-  P2TR = 'p2tr',
+  P2PKH = "p2pkh",
+  P2WPKH = "p2wpkh",
+  P2SH = "p2sh",
+  P2TR = "p2tr",
 }
 
 export type RequestAddressV0Message = {
-  version: MessageVersion.V0,
-  type: V0MessageType.RequestAddress,
-  withMessageSignature?: string | false | null,
-  withExtendedPublicKey?: boolean | null,
-  withScriptType?: V0MessageScriptType | null,
+  version: MessageVersion.V0;
+  type: V0MessageType.RequestAddress;
+  withMessageSignature?: string | false | null;
+  withExtendedPublicKey?: boolean | null;
+  withScriptType?: V0MessageScriptType | null;
 };
 
 export type RequestExtendedPublicKeyV0Message = {
-  version: MessageVersion.V0,
-  type: V0MessageType.RequestExtendedPublicKey,
-  withScriptType?: V0MessageScriptType | null,
+  version: MessageVersion.V0;
+  type: V0MessageType.RequestExtendedPublicKey;
+  withScriptType?: V0MessageScriptType | null;
 };
 
 export type VerifyAddressV0Message = {
-  version: MessageVersion.V0,
-  type: V0MessageType.VerifyAddress,
-  bitcoinAddress: string,
+  version: MessageVersion.V0;
+  type: V0MessageType.VerifyAddress;
+  bitcoinAddress: string;
 };
 
 export type AddressV0Message = {
-  version: MessageVersion.V0,
-  type: V0MessageType.Address,
-  bitcoinAddress: string,
-  signature?: string | null,
-  extendedPublicKey?: string | null,
+  version: MessageVersion.V0;
+  type: V0MessageType.Address;
+  bitcoinAddress: string;
+  signature?: string | null;
+  extendedPublicKey?: string | null;
 };
 
 export type ExtendedPublicKeyV0Message = {
-  version: MessageVersion.V0,
-  type: V0MessageType.ExtendedPublicKey,
-  extendedPublicKey: string,
+  version: MessageVersion.V0;
+  type: V0MessageType.ExtendedPublicKey;
+  extendedPublicKey: string;
 };
 
 export type PaymentRequestV0Message = {
-  version: MessageVersion.V0,
-  type: V0MessageType.PaymentRequest,
-  bitcoinAddress: string,
-  amount: number,
-  label: string | null,
-  message: string | null,
-  slip24: Slip24 | null,
+  version: MessageVersion.V0;
+  type: V0MessageType.PaymentRequest;
+  bitcoinAddress: string;
+  amount: number;
+  label: string | null;
+  message: string | null;
+  slip24: Slip24 | null;
 };
 
 export type CloseV0Message = {
-  version: MessageVersion.V0,
-  type: V0MessageType.Close,
+  version: MessageVersion.V0;
+  type: V0MessageType.Close;
 };
 
 export type Message =
@@ -89,16 +98,16 @@ export function serializeMessage(message: Message) {
 export function parseMessage(value: any): Message {
   let object = value;
 
-  if (typeof object === 'string') {
+  if (typeof object === "string") {
     try {
       object = JSON.parse(object);
     } catch (e) {
-      throw new Error('could not parse as json');
+      throw new Error("could not parse as json");
     }
   }
 
   if (!isObject(object)) {
-    throw new Error('not an object');
+    throw new Error("not an object");
   }
 
   const { version } = object;
@@ -107,23 +116,29 @@ export function parseMessage(value: any): Message {
     const { type } = object;
 
     if (!isOneOf(type, ...Object.values(V0MessageType))) {
-      throw new Error('invalid type');
+      throw new Error("invalid type");
     }
 
     if (type === V0MessageType.RequestAddress) {
       const { withMessageSignature } = object;
       if (!isString(withMessageSignature) && !isNullish(withMessageSignature)) {
-        throw new Error('message signature indicator invalid');
+        throw new Error("message signature indicator invalid");
       }
 
       const { withExtendedPublicKey } = object;
-      if (!isBoolean(withExtendedPublicKey) && !isNullish(withExtendedPublicKey)) {
-        throw new Error('extended public key indicator invalid');
+      if (
+        !isBoolean(withExtendedPublicKey) &&
+        !isNullish(withExtendedPublicKey)
+      ) {
+        throw new Error("extended public key indicator invalid");
       }
 
       const { withScriptType } = object;
-      if (!isOneOf(withScriptType, ...Object.values(V0MessageScriptType)) && !isNullish(withScriptType)) {
-        throw new Error('script type indicator invalid');
+      if (
+        !isOneOf(withScriptType, ...Object.values(V0MessageScriptType)) &&
+        !isNullish(withScriptType)
+      ) {
+        throw new Error("script type indicator invalid");
       }
 
       return {
@@ -135,8 +150,11 @@ export function parseMessage(value: any): Message {
       };
     } else if (type === V0MessageType.RequestExtendedPublicKey) {
       const { withScriptType } = object;
-      if (!isOneOf(withScriptType, ...Object.values(V0MessageScriptType)) && !isNullish(withScriptType)) {
-        throw new Error('script type indicator invalid');
+      if (
+        !isOneOf(withScriptType, ...Object.values(V0MessageScriptType)) &&
+        !isNullish(withScriptType)
+      ) {
+        throw new Error("script type indicator invalid");
       }
 
       return {
@@ -147,7 +165,7 @@ export function parseMessage(value: any): Message {
     } else if (type === V0MessageType.VerifyAddress) {
       const { bitcoinAddress } = object;
       if (!isString(bitcoinAddress)) {
-        throw new Error('bitcoin address missing');
+        throw new Error("bitcoin address missing");
       }
 
       return {
@@ -158,17 +176,17 @@ export function parseMessage(value: any): Message {
     } else if (type === V0MessageType.Address) {
       const { bitcoinAddress } = object;
       if (!isString(bitcoinAddress)) {
-        throw new Error('bitcoin address missing');
+        throw new Error("bitcoin address missing");
       }
 
       const { signature } = object;
       if (!isString(signature) && !isNullish(signature)) {
-        throw new Error('signature invalid');
+        throw new Error("signature invalid");
       }
 
       const { extendedPublicKey } = object;
       if (!isString(extendedPublicKey) && !isNullish(extendedPublicKey)) {
-        throw new Error('extended public key invalid');
+        throw new Error("extended public key invalid");
       }
 
       return {
@@ -181,7 +199,7 @@ export function parseMessage(value: any): Message {
     } else if (type === V0MessageType.ExtendedPublicKey) {
       const { extendedPublicKey } = object;
       if (!isString(extendedPublicKey)) {
-        throw new Error('extended public key missing');
+        throw new Error("extended public key missing");
       }
 
       return {
@@ -192,22 +210,22 @@ export function parseMessage(value: any): Message {
     } else if (type === V0MessageType.PaymentRequest) {
       const { bitcoinAddress } = object;
       if (!isString(bitcoinAddress)) {
-        throw new Error('bitcoin address invalid');
+        throw new Error("bitcoin address invalid");
       }
 
       const { amount } = object;
       if (!isNumber(amount)) {
-        throw new Error('amount invalid');
+        throw new Error("amount invalid");
       }
 
       const { label } = object;
       if (!isString(label) && !isNull(label)) {
-        throw new Error('label invalid');
+        throw new Error("label invalid");
       }
 
       const { message } = object;
       if (!isString(message) && !isNull(message)) {
-        throw new Error('message invalid');
+        throw new Error("message invalid");
       }
 
       const { slip24 } = object;
@@ -219,7 +237,7 @@ export function parseMessage(value: any): Message {
         amount,
         label,
         message,
-        slip24: parseSlip24(slip24)
+        slip24: parseSlip24(slip24),
       };
     } else if (type === V0MessageType.Close) {
       return {
@@ -227,9 +245,9 @@ export function parseMessage(value: any): Message {
         type,
       };
     } else {
-      throw new Error('unsupported type');
+      throw new Error("unsupported type");
     }
   }
 
-  throw new Error('unsupported version');
+  throw new Error("unsupported version");
 }
